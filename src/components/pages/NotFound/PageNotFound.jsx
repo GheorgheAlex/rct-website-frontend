7d3a2b90808828d2a7467e00583d2c990b934ec3
Alexandru Gheorghe <ens18702@adobe.com>
2023-03-23 23:49:38 +0200

    feature/W3RCT-17 Partial commit, ticket WIP.

--- src/components/pages/NotFound/PageNotFound.jsx
+++ src/components/pages/NotFound/PageNotFound.jsx
@@ -1,27 +1,24 @@
 import React from "react";
 import { Helmet } from "react-helmet-async";
 import "./PageNotFound.scss";
-import Logo from "../../../utils/rct-logo.png";
 import { useNavigate } from "react-router-dom";
+import strings from "../../../config/strings";
 const PageNotFound = () => {
   const navigate = useNavigate();
+  const { pageNotFound: pageNotFound } = strings;
   return (
     <>
       <Helmet>
         <title>404 Page not found</title>
       </Helmet>
-      <div className="header">
-        <img src={Logo} className="logoImage" alt="Website logo" />
-      </div>
       <div className="pageNotFoundContent">
-        <h1>404 - Page not found.</h1>
-        <h2>Unfortunately, this page doesn't exist.</h2>
+        <h1>{pageNotFound.errorMessage}</h1>
         <h2
           onClick={() => {
             navigate("/home");
           }}
         >
-          Go back home
+          Mergi pe pagina "Acasa"
         </h2>
       </div>
     </>
