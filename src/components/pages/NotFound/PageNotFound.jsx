7bca29bf4c88c45042065ea79be18d521a696c7a
Alexandru Gheorghe <ens18702@adobe.com>
2023-03-24 20:31:50 +0200

    feature/W3RCT-17 Implemented 404 page according to specs

--- src/components/pages/NotFound/PageNotFound.jsx
+++ src/components/pages/NotFound/PageNotFound.jsx
@@ -3,16 +3,17 @@ import { Helmet } from "react-helmet-async";
 import "./PageNotFound.scss";
 import { useNavigate } from "react-router-dom";
 import strings from "../../../config/strings";
+
 const PageNotFound = () => {
   const navigate = useNavigate();
-  const { pageNotFound: pageNotFound } = strings;
   return (
     <>
       <Helmet>
         <title>404 Page not found</title>
       </Helmet>
       <div className="pageNotFoundContent">
-        <h1>{pageNotFound.errorMessage}</h1>
+        <h1>404 - Not Found</h1>
+        <h1>Se pare ca pagina nu a fost gasita.</h1>
         <h2
           onClick={() => {
             navigate("/home");
