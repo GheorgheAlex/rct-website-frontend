b4771f5903a63884d8b4e87cc6d325320a7ec0c1
Alexandru Gheorghe <ens18702@adobe.com>
2023-03-24 21:43:53 +0200

    feature/W3RCT-17 Added back to main page btn'

--- src/components/pages/NotFound/PageNotFound.jsx
+++ src/components/pages/NotFound/PageNotFound.jsx
@@ -3,6 +3,7 @@ import { Helmet } from "react-helmet-async";
 import "./PageNotFound.scss";
 import { useNavigate } from "react-router-dom";
 import SearchOffIcon from '@mui/icons-material/SearchOff';
+import Button from "@mui/material/Button";
 
 const PageNotFound = () => {
   const navigate = useNavigate();
@@ -15,13 +16,15 @@ const PageNotFound = () => {
         <SearchOffIcon className='notFoundIcon'/>
         <h1>404 - Not Found</h1>
         <h1>Se pare că pagina nu a fost găsită.</h1>
-        <h2
-          onClick={() => {
-            navigate("/home");
-          }}
-        >
-          Mergi pe pagina "Acasă"
-        </h2>
+          <Button
+              variant="contained"
+              color="secondary"
+              className="menu-button"
+              onClick={() => {
+                  navigate("/home");
+              }}>
+              Pagina principală
+          </Button>
       </div>
     </>
   );
