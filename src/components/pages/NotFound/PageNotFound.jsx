00a6cb9b6fdec008fe323c24c065558a32abd271
Alexandru Gheorghe <ens18702@adobe.com>
2023-03-24 21:24:42 +0200

    feature/W3RCT-17 Made changes according to PR comments

--- src/components/pages/NotFound/PageNotFound.jsx
+++ src/components/pages/NotFound/PageNotFound.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Helmet } from "react-helmet-async";
 import "./PageNotFound.scss";
 import { useNavigate } from "react-router-dom";
-import strings from "../../../config/strings";
+import SearchOffIcon from '@mui/icons-material/SearchOff';
 
 const PageNotFound = () => {
   const navigate = useNavigate();
@@ -12,14 +12,15 @@ const PageNotFound = () => {
         <title>404 Page not found</title>
       </Helmet>
       <div className="pageNotFoundContent">
+        <SearchOffIcon className='notFoundIcon'/>
         <h1>404 - Not Found</h1>
-        <h1>Se pare ca pagina nu a fost gasita.</h1>
+        <h1>Se pare că pagina nu a fost găsită.</h1>
         <h2
           onClick={() => {
             navigate("/home");
           }}
         >
-          Mergi pe pagina "Acasa"
+          Mergi pe pagina "Acasă"
         </h2>
       </div>
     </>
